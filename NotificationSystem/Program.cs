67ce87ba8884ac6126a75f3f2e87d29d9995eaf3
Ayu Kumar Dhawan <adhawan@ee.iitr.ac.in>
2025-12-21 17:09:25 +0530

    Added PaymentGateway Module Design

---
+++ NotificationSystem/Program.cs
@@ -0,0 +1,243 @@
+﻿
+
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+
+
+public class NotificationSystem{
+
+
+  /// <summary>
+  /// Notifcation Decorators
+  /// </summary>
+  public interface INotification
+  {
+    public string GetContent();
+  }
+
+  public class SimpleNotification : INotification
+  {
+    public string _content;
+    public SimpleNotification(string content)
+    {
+      _content = content;
+    }
+    public string GetContent()
+    {
+      return _content;
+    }
+  }
+
+  public abstract class NotificationDecorator : INotification
+  {
+    public INotification _notification;
+    public NotificationDecorator(INotification notification)
+    {
+      _notification = notification;
+    }
+    public virtual string GetContent()
+    {
+      return _notification.GetContent();
+    }
+  }
+
+
+  public class TimeStampNotificationDecorator : NotificationDecorator
+  {
+    public TimeStampNotificationDecorator(INotification notification) : base(notification) { }
+    public override string GetContent()
+    {
+      return $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {_notification.GetContent()}";
+    }
+  }
+
+
+  public class SignatureNotificationDecorator : NotificationDecorator
+  {
+    public SignatureNotificationDecorator(INotification notification) : base(notification) { }
+    public override string GetContent()
+    {
+      return "Sent By Admin(sign)" + $"{_notification.GetContent()}";
+    }
+  }
+
+
+  public interface IObserver
+  {
+    public void Update();
+  }
+
+  public interface IObservable
+  {
+    public void Add(IObserver observer);
+    public void Remove(IObserver observer);
+
+    public void NotifyAllObserver();
+  }
+
+
+
+  public class NotificatinObservable : IObservable
+  {
+    private List<IObserver> _observers = new List<IObserver>();
+    public INotification currentNotification;
+
+    public void Add(IObserver observer)
+    {
+      _observers.Add(observer);
+    }
+
+    public void Remove(IObserver observer)
+    {
+      _observers.Remove(observer);
+    }
+
+    public void NotifyAllObserver()
+    {
+      foreach (var observer in _observers)
+      {
+        observer.Update();
+      }
+    }
+
+    public void sendNotification(INotification notification)
+    {
+      currentNotification = notification;
+      NotifyAllObserver();
+    }
+
+    public string GetNotificationContent()
+    {
+      return currentNotification.GetContent();
+    }
+  }
+
+
+  public class NotificationService
+  {
+    public NotificatinObservable notificatinObservable;
+
+    private static readonly Lazy<NotificationService> _instance = new Lazy<NotificationService>(() => new NotificationService(notificatinObservable: new NotificatinObservable()));
+
+    public static NotificationService Instance = _instance.Value;
+    public NotificationService(NotificatinObservable notificatinObservable)
+    {
+      this.notificatinObservable = notificatinObservable;
+    }
+
+    private IList<INotification> _notifications = new List<INotification>();
+
+    public void sendNotification(INotification notification)
+    {
+      _notifications.Add(notification);
+      notificatinObservable.sendNotification(notification);
+    }
+  }
+
+
+
+  public class Logger : IObserver
+  {
+    private NotificatinObservable _notificatinObservable;
+
+    public Logger()
+    {
+      this._notificatinObservable = NotificationService.Instance.notificatinObservable;
+    }
+
+    public Logger(NotificatinObservable notificatinObservable)
+    {
+      _notificatinObservable = notificatinObservable;
+      _notificatinObservable.Add(this);
+    }
+
+    public void Update()
+    {
+      Console.WriteLine($"Logging new Notification :\n {_notificatinObservable.currentNotification.GetContent()}");
+    }
+  }
+
+  /*============================
+    Strategy Pattern Components (Concrete Observer 2)
+  =============================*/
+
+  // Abstract class for different Notification Strategies.
+  public interface INotificationStrategy
+  {
+    public void sendNotification(string content);
+  };
+
+
+  public class EmailNotificationStrategy : INotificationStrategy
+  {
+    private string _emailAddress;
+    public EmailNotificationStrategy(string emailAddress)
+    {
+      _emailAddress = emailAddress;
+    }
+    public void sendNotification(string content)
+    {
+      Console.WriteLine($"Sending Email Notification to emailID : {_emailAddress} with content: {content}");
+    }
+  }
+
+  public class SMSNotificationStrategy : INotificationStrategy
+  {
+    private string _phoneNumber;
+    public SMSNotificationStrategy(string phoneNumber)
+    {
+      _phoneNumber = phoneNumber;
+    }
+    public void sendNotification(string content)
+    {
+      Console.WriteLine($"Sending SMS Notification to Phone Number : {_phoneNumber} with content: {content}");
+    }
+  }
+
+
+  public class NotificationEngine : IObserver
+  {
+    private NotificatinObservable _notificatinObservable;
+    private List<INotificationStrategy> _notificationStrategies = new List<INotificationStrategy>();
+
+    public NotificationEngine()
+    {
+      this._notificatinObservable = NotificationService.Instance.notificatinObservable;
+      _notificatinObservable.Add(this);
+    }
+
+    public void AddNotifcationStrategy(INotificationStrategy notificationStrategy)
+    {
+      _notificationStrategies.Add(notificationStrategy);
+    }
+
+    public void Update()
+    {
+      string content = _notificatinObservable.GetNotificationContent();
+      foreach (var strategy in _notificationStrategies)
+      {
+        strategy.sendNotification(content);
+      }
+    }
+  }
+
+  static void Main(string[] args){
+    // Create NotificationService.
+    NotificationService notificationService=  NotificationService.Instance;
+     
+      // Create Observers
+      Logger logger = new Logger();
+      NotificationEngine notificationEngine = new NotificationEngine();
+    Console.WriteLine("Observers Registered");
+
+    notificationEngine.AddNotifcationStrategy(new EmailNotificationStrategy("dhawanayu@gmail.com"));
+    notificationEngine.AddNotifcationStrategy(new SMSNotificationStrategy("+7983168691"));
+    INotification notification1 = new SimpleNotification("This is a simple notification");
+    notificationService.sendNotification(notification1);
+    notification1 = new TimeStampNotificationDecorator(notification1);
+    notification1 = new SignatureNotificationDecorator(notification1);
+    notificationService.sendNotification(notification1);
+  }
+}
+
---
+++ PaymentGatewayDesign/Program.cs
@@ -0,0 +1,297 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
+
+public class PaymentRequest
+{
+  public string Sender { get; set; }
+  public string Receiver { get; set; }
+  public int Amount { get; set; }
+  public string Currency { get; set; }
+
+  public PaymentRequest(string sender, string receiver, int amount, string currency)
+  {
+    Sender = sender;
+    Receiver = receiver;
+    Amount = amount;
+    Currency = currency;
+  }
+}
+
+public enum GateWayType
+{
+  Paytm,
+  RazorPay
+}
+
+
+public interface IBankingSystem
+{
+  public bool ProcessPayment(PaymentRequest req);
+}
+
+
+public class PaytmBakingSystem : IBankingSystem
+{
+  private static Random rand = new Random();
+  public bool ProcessPayment(PaymentRequest req)
+  {
+    //Simulating 20% success rate
+    return rand.Next(0, 100) <=20; // Simulate success/failure
+  }
+}
+
+public class RazorpayBankingSystem: IBankingSystem
+{
+  //70% Success rate
+  private Random rand = new Random();
+  public bool ProcessPayment(PaymentRequest req)
+  {
+    //Simulating 10% success rate
+    return rand.Next(0, 100) <=70; // Simulate success/failure
+  }
+}
+/// <summary>
+/// Abstract class for Payment Gateway (Template design pattern)
+/// </summary>
+public abstract class PaymentGateway
+{
+  public IBankingSystem? _bankingSystem;
+  public PaymentGateway()
+  {
+    _bankingSystem = null;
+  }
+  public virtual bool ProcessPayment(PaymentRequest req)
+  {
+    if (!validatePaymentRequest(req))
+    {
+      Console.WriteLine($"payment validation failed for {req.Sender} .");
+      return false;
+    }
+    if (!initiatePayment(req))
+    {
+      Console.WriteLine($"Payment initiate failed for {req.Sender}.");
+      return false;
+    }
+
+    if (!confirmpayment(req))
+    {
+       Console.WriteLine($"Payment Confirmation failed for {req.Sender}.");
+      return false;
+    }
+    return true;
+  }
+
+  public abstract bool validatePaymentRequest(PaymentRequest req);
+  public abstract bool initiatePayment(PaymentRequest req);
+  public abstract bool confirmpayment(PaymentRequest req);
+
+}
+
+
+public class PaytmPaymentGateway : PaymentGateway
+{
+  public PaytmPaymentGateway()
+  {
+    this._bankingSystem = new PaytmBakingSystem();
+  }
+  public override bool validatePaymentRequest(PaymentRequest req)
+  {
+
+    Console.WriteLine($"Validting Payment Request for :{req.Sender}");
+    if (req.Amount <= 0 && "INR".Equals(req.Currency))
+    {
+      return false;
+    }
+    return true;
+  }
+
+  public override bool initiatePayment(PaymentRequest req)
+  {
+    Console.WriteLine($"Initiating Payment for :{req.Sender} to: {req.Receiver} Amount: {req.Amount} {req.Currency}");
+    if (_bankingSystem.ProcessPayment(req))
+    {
+      Console.WriteLine($"Payment Initiated successfully for :{req.Sender}");
+      return true;
+    }
+    return false;
+  }
+
+  public override bool confirmpayment(PaymentRequest req)
+  {
+    Console.WriteLine("[Paytm] Confirming payment for " + req.Sender + ".");
+    return true;
+  }
+}
+
+
+/// <summary>
+/// Razorpay Payment Gateway
+/// </summary>
+
+
+
+public class RazorPayPaymentGateway : PaymentGateway
+{
+  public RazorPayPaymentGateway()
+  {
+    this._bankingSystem = new RazorpayBankingSystem();
+  }
+  public override bool validatePaymentRequest(PaymentRequest req)
+  {
+
+    Console.WriteLine($"Validting Payment Request for :{req.Sender}");
+    if (req.Amount <= 0 && "INR".Equals(req.Currency))
+    {
+      return false;
+    }
+    return true;
+  }
+
+  public override bool initiatePayment(PaymentRequest req)
+  {
+    Console.WriteLine($"Initiating Payment for :{req.Sender} to: {req.Receiver} Amount: {req.Amount} {req.Currency}");
+    if (_bankingSystem.ProcessPayment(req))
+    {
+      Console.WriteLine($"Payment Initiated successfully for :{req.Sender}");
+      return true;
+    }
+    return false;
+  }
+
+  public override bool confirmpayment(PaymentRequest req)
+  {
+    Console.WriteLine("[RazorPay] Confirming payment for " + req.Sender + ".");
+    return true;
+  }
+}
+
+
+
+/// <summary>
+/// Proxy class for Payment Service
+/// </summary>
+public class PaymentGatewayProxy : PaymentGateway
+{
+  private PaymentGateway _realPaymentGateway;
+
+  private int _maxRetries;
+
+  public PaymentGatewayProxy(PaymentGateway pg, int maxRetries)
+  {
+    _realPaymentGateway = pg;
+    _maxRetries = maxRetries;
+  }
+
+  public override bool ProcessPayment(PaymentRequest paymentRequest)
+  {
+    bool result = false;
+    for (int attempt = 0; attempt < _maxRetries; attempt++)
+    {
+      if (attempt > 0)
+      {
+        Console.WriteLine($"[Proxy] Retrying Payment (attempt: {attempt})");
+      }
+
+      result = _realPaymentGateway.ProcessPayment(paymentRequest);
+      if (result)
+      {
+        Console.WriteLine("[Proxy] Payment succeeded for " + paymentRequest.Sender + " on attempt " + (attempt + 1) + ".");
+        break;
+      }
+      if (!result)
+      {
+        Console.WriteLine("[Proxy] Payment failed after " + attempt
+                + " attempts for " + paymentRequest.Sender + ".");
+      }
+      return result;
+    }
+    return false;
+  }
+
+  public override bool validatePaymentRequest(PaymentRequest req)
+  {
+    return _realPaymentGateway.validatePaymentRequest(req);
+  }
+
+  public override bool initiatePayment(PaymentRequest req)
+  {
+    return _realPaymentGateway.initiatePayment(req);
+  }
+
+  public override bool confirmpayment(PaymentRequest req)
+  {
+    return _realPaymentGateway.confirmpayment(req);
+  }
+}
+
+public class GatewayFactory
+{
+  private GatewayFactory(){}
+  private readonly static Lazy<GatewayFactory> factoryInstance = new Lazy<GatewayFactory>(() => new GatewayFactory());
+  public static GatewayFactory getInstance = factoryInstance.Value;
+  public PaymentGateway GetPaymentGatewayInstance(GateWayType type){
+    if (GateWayType.RazorPay == type)
+    {
+      PaymentGateway paymentGateway = new PaytmPaymentGateway();
+      return new PaymentGatewayProxy(paymentGateway, 3);
+    }
+    else if (GateWayType.Paytm == type)
+    {
+      PaymentGateway paymentGateway = new RazorPayPaymentGateway();
+      return new PaymentGatewayProxy(paymentGateway, 3);
+    }
+    return new PaymentGatewayProxy(new PaytmPaymentGateway(), 3);
+  }
+}
+
+
+public class PaymentService
+{
+  public PaymentGateway _paymentGateway;
+  private PaymentService(){}
+  public static readonly Lazy<PaymentService> instance =
+  new Lazy<PaymentService>(() => new PaymentService());
+
+  public static PaymentService getInstance = instance.Value;
+
+  public void SetGateway(PaymentGateway pg)
+  {
+    _paymentGateway = pg;
+  }
+
+  public bool ProcessPayment(PaymentRequest pr)
+  {
+    if (_paymentGateway == null)
+    {
+      throw new Exception("Payment Gateway not set.");
+    }
+    return _paymentGateway.ProcessPayment(pr);
+  }
+}
+
+public class PaymentController
+{
+  private PaymentController(){}
+  private readonly static Lazy<PaymentController> paymentController = new Lazy<PaymentController>(() => new PaymentController());
+  public static PaymentController getInstance = paymentController.Value;
+  public bool handlePayment(GateWayType gt, PaymentRequest request)
+  {
+    PaymentGateway pg = GatewayFactory.getInstance.GetPaymentGatewayInstance(gt);
+    PaymentService.getInstance.SetGateway(pg);
+    return PaymentService.getInstance.ProcessPayment(request);
+  }
+}
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var newPaymentRequest = new PaymentRequest("Alice", "Bob", 1000, "INR");
+    var paymentController = PaymentController.getInstance;
+    bool paymentStatus = paymentController.handlePayment(GateWayType.Paytm, newPaymentRequest);
+    Console.WriteLine($"Payment Status: {paymentStatus}");  
+  }
+}
